b016d2481cc66414a19ac2dd6f1c25d7605a4f26
Nahuel Jose Roldan <nahuel.developer@gmail.com>
2023-10-18 17:24:44 -0300

    Add Form, Section and Navigation Stack.

--- swiftui-playground/swiftui-playground/ContentView.swift
+++ swiftui-playground/swiftui-playground/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    Text("Hello, world!")
+                }
+            }
+            .navigationTitle("SwiftUI")
+//            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
     }
 }
 
