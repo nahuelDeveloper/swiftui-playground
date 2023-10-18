85496af9d1efe89efce551def05ee0842ee113d5
Nahuel Jose Roldan <nahuel.developer@gmail.com>
2023-10-18 17:13:32 -0300

    Add Xcode project, add gitignore.

---
+++ swiftui-playground/swiftui-playground/swiftui_playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_playgroundApp.swift
+//  swiftui-playground
+//
+//  Created by Nahuel Jose Roldan on 18/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
